9464d80be078b5481519ebc7df28be405b6e93b4
sauda00 <116629853+sauda00@users.noreply.github.com>
2023-05-15 14:12:40 +0600

    link (#8)
    Co-authored-by: sauda00 <saudakalybekova@gmil.come>

--- src/pages/Courses/latest/Latest.js
+++ src/pages/Courses/latest/Latest.js
@@ -17,139 +17,38 @@ const Latest = () => {
 
                  <div className="latest--title">
 
-                     <div className="latest--title__block">
-                         <img className='read' src={Rectangle} alt=""/>
-                         <div className='latest--title__block--one'>
-                             <p>Статьи</p>
-                             <h1>Мы подготовили подборку самых популярных курсов по
-                                 направлению Java в IBS Training Center. </h1>
-                             <div className='latest--title__block--one__tx'>
-                                 <div className='latest--title__block--one__tx--not'>
-                                     <img src={read} alt=""/>
-                                     <a href="src/HomePage/Articles/articles#">Читать</a>
-                                 </div>
-                                 <div>
-                                     <p>01.02.2022</p>
-                                 </div>
-                             </div>
+
+
+                         <div className="latest--title__text">
+                         <h5>Статьи</h5>
+                         <h1>Мы подготовили подборку  самых
+                             популярных курсов  по направлению Java в IBS Training Center. </h1>
+                         <div className="latest--title__text--icon">
+                             <button><BsFillClipboardMinusFill/>Читать</button>
+                             <h4>21.09.2022</h4>
                          </div>
+
                      </div>
-                     <div className="latest--title__block">
-                         <img className='read' src={Rectangle} alt=""/>
-                         <div className='latest--title__block--one'>
-                             <p>Статьи</p>
-                             <h1>Мы подготовили подборку самых популярных курсов по
-                                 направлению Java в IBS Training Center. </h1>
-                             <div className='latest--title__block--one__tx'>
-                                 <div className='latest--title__block--one__tx--not'>
-                                     <img src={read} alt=""/>
-                                     <a href="src/HomePage/Articles/articles#">Читать</a>
-                                 </div>
-                                 <div>
-                                     <p>01.02.2022</p>
-                                 </div>
-                             </div>
+                     <div className="latest--title__text">
+                         <h5>Статьи</h5>
+                         <h1>Мы подготовили подборку самых
+                             популярных курсов  по направлению Java в IBS  Training Center. </h1>
+                         <div className="latest--title__text--icon">
+                             <button><BsFillClipboardMinusFill/>Читать</button>
+                             <h4>21.09.2022</h4>
                          </div>
+
                      </div>
-                     <div className="latest--title__block">
-                         <img className='read' src={Rectangle} alt=""/>
-                         <div className='latest--title__block--one'>
-                             <p>Статьи</p>
-                             <h1>Мы подготовили подборку самых популярных курсов по
-                                 направлению Java в IBS Training Center. </h1>
-                             <div className='latest--title__block--one__tx'>
-                                 <div className='latest--title__block--one__tx--not'>
-                                     <img src={read} alt=""/>
-                                     <a href="src/HomePage/Articles/articles#">Читать</a>
-                                 </div>
-                                 <div>
-                                     <p>01.02.2022</p>
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-                     <div className="latest--title__block">
-                         <img className='read' src={Rectangle} alt=""/>
-                         <div className='latest--title__block--one'>
-                             <p>Статьи</p>
-                             <h1>Мы подготовили подборку самых популярных курсов по
-                                 направлению Java в IBS Training Center. </h1>
-                             <div className='latest--title__block--one__tx'>
-                                 <div className='latest--title__block--one__tx--not'>
-                                     <img src={read} alt=""/>
-                                     <a href="src/HomePage/Articles/articles#">Читать</a>
-                                 </div>
-                                 <div>
-                                     <p>01.02.2022</p>
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-                     <div className="latest--title__block">
-                         <img className='read' src={Rectangle} alt=""/>
-                         <div className='latest--title__block--one'>
-                             <p>Статьи</p>
-                             <h1>Мы подготовили подборку самых популярных курсов по
-                                 направлению Java в IBS Training Center. </h1>
-                             <div className='latest--title__block--one__tx'>
-                                 <div className='latest--title__block--one__tx--not'>
-                                     <img src={read} alt=""/>
-                                     <a href="src/HomePage/Articles/articles#">Читать</a>
-                                 </div>
-                                 <div>
-                                     <p>01.02.2022</p>
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-                     <div className="latest--title__block">
-                         <img className='read' src={Rectangle} alt=""/>
-                         <div className='latest--title__block--one'>
-                             <p>Статьи</p>
-                             <h1>Мы подготовили подборку самых популярных курсов по
-                                 направлению Java в IBS Training Center. </h1>
-                             <div className='latest--title__block--one__tx'>
-                                 <div className='latest--title__block--one__tx--not'>
-                                     <img src={read} alt=""/>
-                                     <a href="src/HomePage/Articles/articles#">Читать</a>
-                                 </div>
-                                 <div>
-                                     <p>01.02.2022</p>
-                                 </div>
-                             </div>
-                         </div>
+                     <div className="latest--title__text">
+                     <h5>Статьи</h5>
+                     <h1>Мы подготовили подборку  самых
+                         популярных курсов по направлению Java в IBS Training Center. </h1>
+                     <div className="latest--title__text--icon">
+                         <button><BsFillClipboardMinusFill/>Читать</button>
+                         <h4>21.09.2022</h4>
                      </div>
 
-                     {/*    <div className="latest--title__text">*/}
-                 {/*        <h5>Статьи</h5>*/}
-                 {/*        <h1>Мы подготовили подборку  самых*/}
-                 {/*            популярных курсов  по направлению Java в IBS Training Center. </h1>*/}
-                 {/*        <div className="latest--title__text--icon">*/}
-                 {/*            <button><BsFillClipboardMinusFill/>Читать</button>*/}
-                 {/*            <h4>21.09.2022</h4>*/}
-                 {/*        </div>*/}
-
-                 {/*    </div>*/}
-                 {/*    <div className="latest--title__text">*/}
-                 {/*        <h5>Статьи</h5>*/}
-                 {/*        <h1>Мы подготовили подборку самых*/}
-                 {/*            популярных курсов  по направлению Java в IBS  Training Center. </h1>*/}
-                 {/*        <div className="latest--title__text--icon">*/}
-                 {/*            <button><BsFillClipboardMinusFill/>Читать</button>*/}
-                 {/*            <h4>21.09.2022</h4>*/}
-                 {/*        </div>*/}
-
-                 {/*    </div>*/}
-                 {/*    <div className="latest--title__text">*/}
-                 {/*    <h5>Статьи</h5>*/}
-                 {/*    <h1>Мы подготовили подборку  самых*/}
-                 {/*        популярных курсов по направлению Java в IBS Training Center. </h1>*/}
-                 {/*    <div className="latest--title__text--icon">*/}
-                 {/*        <button><BsFillClipboardMinusFill/>Читать</button>*/}
-                 {/*        <h4>21.09.2022</h4>*/}
-                 {/*    </div>*/}
-
-                 {/*</div>*/}
+                 </div>
                  </div>
 
                 </div>
