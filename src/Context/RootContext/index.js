d23895529d806fe9ecf8d12b9e7dc7bbede3f1cf
sanychkenjebekov <121374323+sanychkenjebekov@users.noreply.github.com>
2023-05-23 17:10:25 +0600

    notification (#40)

--- src/Context/RootContext/index.js
+++ src/Context/RootContext/index.js
@@ -3,10 +3,17 @@ import {LoginContext} from "../index";
 
 const RootContext = ({children}) => {
     const [login,setLogin] = useState('Неизвестно')
+    const [smsValue,setSmsValue] = useState('У вас нету коментарии !')
+    const [notification,setNotification] = useState('У вас ещё нету уведомлении !')
+
     return (
         <LoginContext.Provider value={{
             login,
-            setLogin
+            setLogin,
+            smsValue,
+            setSmsValue,
+            notification,
+            setNotification
         }}>
             {children}
         </LoginContext.Provider>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,29 +3,31 @@ import logo from '../../img/логотип.svg'
 import {Link, NavLink} from "react-router-dom";
 import {IoIosNotificationsOutline} from "react-icons/io";
 
+
+
 const Header = () => {
-    const spans =(e)=>{
+    const spans = (e) => {
         const p1 = document.querySelector('.p1')
         const p2 = document.querySelector('.p2')
         const p3 = document.querySelector('.p3')
         const burger = document.querySelector('.burger')
 
-        if(!e.target.classList.contains('active')){
+        if (!e.target.classList.contains('active')) {
             e.target.classList.add('active')
-           p1.style.transform = 'rotate(45deg)'
-           p1.style.margin = '18px 0 0 0'
-           p2.style.opacity = '0'
-           p3.style.transform = 'rotate(-45deg)'
-           p3.style.margin = '-16px 0 0 0'
-            burger.style.display='block'
-        }else {
+            p1.style.transform = 'rotate(45deg)'
+            p1.style.margin = '18px 0 0 0'
+            p2.style.opacity = '0'
+            p3.style.transform = 'rotate(-45deg)'
+            p3.style.margin = '-16px 0 0 0'
+            burger.style.display = 'block'
+        } else {
             e.target.classList.remove('active')
             p1.style.transform = 'none'
             p1.style.margin = '5px 0'
             p2.style.opacity = '1'
             p3.style.transform = 'none'
             p3.style.margin = '5px 0'
-            burger.style.display='none'
+            burger.style.display = 'none'
         }
     }
 
@@ -43,14 +45,18 @@ const Header = () => {
                     </div>
                     <div className="header--podpis">
                         <NavLink to={'/войти'}>Войти</NavLink>
-                        <NavLink to={'/подписаться '}><button>Подписаться</button></NavLink>
+                        <NavLink to={'/подписаться '}>
+                            <button>Подписаться</button>
+                        </NavLink>
                     </div>
                     <div className="header--voitiAll">
-                    <div className="header--voitiAll__voiti">
-                        <NavLink to={'/уведомление'}><IoIosNotificationsOutline className='voitiIcon'/></NavLink>
-                        <NavLink to={'/профиль'}><button>Профиль</button></NavLink>
+                        <div className="header--voitiAll__voiti">
+                            <NavLink className='notific' to={'/уведомление'}><IoIosNotificationsOutline className='voitiIcon'/></NavLink>
+                            <NavLink to={'/профиль'}>
+                                <button>Профиль</button>
+                            </NavLink>
+                        </div>
                     </div>
-                </div>
                 </div>
                 <div className="spans" onClick={spans}>
                     <p className='p1'></p>
@@ -63,17 +69,22 @@ const Header = () => {
                         <NavLink to={'/нашиКурсы'}>Наши курсы</NavLink>
                         <NavLink to={'/оНас'}>О нас</NavLink>
                         <div className="burger--menu__podpisAll">
-                        <div className="burger--menu__podpisAll--podpis">
-                            <NavLink to={'/войти'}>Войти</NavLink>
-                            <NavLink to={'/подписаться '}><button>Подписаться</button></NavLink>
+                            <div className="burger--menu__podpisAll--podpis">
+                                <NavLink to={'/войти'}>Войти</NavLink>
+                                <NavLink to={'/подписаться '}>
+                                    <button>Подписаться</button>
+                                </NavLink>
+                            </div>
                         </div>
-                    </div>
                         <div className="burger--menu__voitiAll">
-                        <div className="burger--menu__voitiAll--voiti">
-                            <NavLink to={'/уведомление'}><IoIosNotificationsOutline className='voitiIcon'/></NavLink>
-                            <NavLink to={'/профиль'}><button>Профиль</button></NavLink>
+                            <div className="burger--menu__voitiAll--voiti">
+                                <NavLink className='notific' to={'/уведомление'}><IoIosNotificationsOutline
+                                    className='voitiIcon'/></NavLink>
+                                <NavLink to={'/профиль'}>
+                                    <button>Профиль</button>
+                                </NavLink>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
 
