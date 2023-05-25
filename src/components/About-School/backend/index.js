b71d526567823dbbafc376fb34dcf13e441f9662
sanjar <san4o.myrzakmatov@gmail.com>
2023-05-24 20:46:11 -0800

    alls

--- src/components/About-School/backend/index.js
+++ src/components/About-School/backend/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useState} from 'react';
+import {Link, useNavigate} from "react-router-dom";
 import {BsCreditCard} from "react-icons/bs";
 
 
 
 function BackSend () {
+    const nav = useNavigate()
+    const [redirectToFront, setRedirectToFront] = useState(false);
     const oplata = (e) => {
-        const oplata = document.querySelector('.oplata')
         const i1 = document.querySelector('.i1')
         const i2 = document.querySelector('.i2')
         const i3 = document.querySelector('.i3')
@@ -15,31 +16,102 @@ function BackSend () {
         const i6 = document.querySelector('.i6')
         const i7 = document.querySelector('.i7')
         const i8 = document.querySelector('.i8')
-        // e.preventDefault()
+        const i9 = document.querySelector('.i9')
+        const i10 = document.querySelector('.i10')
+        e.preventDefault()
         if (i1.value === '' || i2.value === '' || i3.value === '' || i4.value === '' || i5.value === '' || i6.value === '' || i7.value === '' || i8.value === '') {
-            oplata.style.display = 'none'
-            // i1.style.border = '1px solid red'
-            // i2.style.border = '1px solid red'
-            // i3.style.border = '1px solid red'
-            // i4.style.border = '1px solid red'
-            // i5.style.border = '1px solid red'
-            // i6.style.border = '1px solid red'
-            // i7.style.border = '1px solid red'
+            i1.style.border = '1px solid red'
+            i2.style.border = '1px solid red'
+            i3.style.border = '1px solid red'
+            i4.style.border = '1px solid red'
+            i5.style.border = '1px solid red'
+            i6.style.border = '1px solid red'
+            i7.style.border = '1px solid red'
             // i8.style.border = '1px solid red'
+            i9.parentNode.style.border = '1px solid red'
+            i10.parentNode.style.border = '1px solid red'
         } else {
-            oplata.style.display = 'block'
-            // i1.style.border = '1px solid white'
-            // i2.style.border = '1px solid white'
-            // i3.style.border = '1px solid white'
-            // i4.style.border = '1px solid white'
-            // i5.style.border = '1px solid white'
-            // i6.style.border = '1px solid white'
-            // i7.style.border = '1px solid white'
+            i1.style.border = '1px solid white'
+            i2.style.border = '1px solid white'
+            i3.style.border = '1px solid white'
+            i4.style.border = '1px solid white'
+            i5.style.border = '1px solid white'
+            i6.style.border = '1px solid white'
+            i7.style.border = '1px solid white'
             // i8.style.border = '1px solid white'
-            window.setTimeout(() => {
-                oplata.style.display = 'none'
+            i9.parentNode.style.border = '1px solid white'
+            i10.parentNode.style.border = '1px solid white'
+            setRedirectToFront(true)
+        }
+    }
+    if(redirectToFront === true){
+        nav('/front')
+        return null
+    }
 
-            }, 3000)
+    function oplata2(){
+        const i = document.querySelector('input')
+        const i1 = document.querySelector('.i1')
+        const i2 = document.querySelector('.i2')
+        const i3 = document.querySelector('.i3')
+        const i4 = document.querySelector('.i4')
+        const i5 = document.querySelector('.i5')
+        const i6 = document.querySelector('.i6')
+        const i7 = document.querySelector('.i7')
+        const i8 = document.querySelector('.i8')
+        const i9 = document.querySelector('.i9')
+        const i10 = document.querySelector('.i10')
+        if (i.value.trim() === '') {
+            i.style.border = '1px solid red'
+        } else {
+            i.style.border = '1px solid white'
+        }
+        if (i1.value.trim() === '') {
+            i1.style.border = '1px solid red'
+        } else {
+            i1.style.border = '1px solid white'
+        }
+        if (i2.value === '') {
+            i2.style.border = '1px solid red'
+        } else {
+            i2.style.border = '1px solid white'
+        }
+        if (i3.value === '') {
+            i3.style.border = '1px solid red'
+        } else {
+            i3.style.border = '1px solid white'
+        }
+        if (i4.value === '') {
+            i4.style.border = '1px solid red'
+        } else {
+            i4.style.border = '1px solid white'
+        }
+        if (i5.value === '') {
+            i5.style.border = '1px solid red'
+        } else {
+            i5.style.border = '1px solid white'
+        }
+        if (i6.value === '') {
+            i6.style.border = '1px solid red'
+        } else {
+            i6.style.border = '1px solid white'
+        }
+        if (i7.value === '') {
+            i7.style.border = '1px solid red'
+        } else {
+            i7.style.border = '1px solid white'
+        }
+        if (i8.value === '') {
+            i8.style.border = '1px solid red'
+        } else {
+            i8.style.border = '1px solid white'
+        }
+        if (i9.checked || i10.checked) {
+            i9.parentNode.style.border = '1px solid white'
+            i10.parentNode.style.border = '1px solid white'
+        } else {
+            i9.parentNode.style.border = '1px solid red'
+            i10.parentNode.style.border = '1px solid red'
         }
     }
 
@@ -66,33 +138,33 @@ function BackSend () {
                         <div className="god--about">
                             <form action="#">
                                 <div className="god--about__input">
-                                    <input className='i1' required placeholder='Ф.И.О*' name='name' type="text"/>
-                                    <input className='i2' required placeholder='Телефон*' name='tel' type="number"/>
-                                    <input className='i3' required placeholder='E mail*' name='email' type="email"/>
-                                    <input className='i4' required placeholder='Пароль' type="password"/>
+                                    <input onInput={oplata2} className='i1' required placeholder='Ф.И.О*' name='name' type="text"/>
+                                    <input onInput={oplata2} className='i2' required placeholder='Телефон*' name='tel' type="number"/>
+                                    <input onInput={oplata2} className='i3' required placeholder='E mail*' name='email' type="email"/>
+                                    <input onInput={oplata2} className='i4' required placeholder='Пароль' type="password"/>
                                     <p>Выберите платежную карту</p>
                                     <div className="god--about__input--radio">
                                         <div className="god--about__input--radio__one">
-                                            <input required name='radio' type="radio"/>
+                                            <input onInput={oplata2} className='i9' required name='radio' type="radio"/>
                                             <h2>Visa</h2>
                                         </div>
                                         <div className="god--about__input--radio__one">
-                                            <input required name='radio' type="radio"/>
+                                            <input onInput={oplata2} className='i10' required name='radio' type="radio"/>
                                             <h2>MasterCard</h2>
                                         </div>
                                     </div>
-                                    <input className='i5' required placeholder='Номер карты *' name='karta'
+                                    <input onInput={oplata2} className='i5' required placeholder='Номер карты *' name='karta'
                                            type="number"/>
                                     <div className="god--about__input--cvc">
-                                        <input className='i6' required placeholder='ММ/ГГ *' type="tel"/>
-                                        <input className='i7' required placeholder='CVC *' type="text"/>
+                                        <input onInput={oplata2} className='i6' required placeholder='ММ/ГГ *' type="tel"/>
+                                        <input onInput={oplata2} className='i7' required placeholder='CVC *' type="text"/>
                                     </div>
                                     <div className="god--about__input--chekbox">
-                                        <input className='i8' required type="checkbox"/>
+                                        <input onInput={oplata2} className='i8' required type="checkbox"/>
                                         <h5>Я ознакомился и согласен с Условиями оказания услуг</h5>
                                     </div>
                                     <button onClick={oplata} style={{cursor: 'pointer',padding:'12px 15px'}} type='submit'>Оплатить
-                                        <Link style={{color:'white',margin:'0 0 0 10px'}}  to={'/front'}>220.00$</Link>
+                                       220.00$
                                     </button>
                                 </div>
                                 <div className="oplata" style={{
